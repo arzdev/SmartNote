f5a6dba225ae9d38c84c34c88eae94b1ca9f8311
TylerGarrett98 <tmgarret@ucsd.edu>
2020-05-04 20:05:30 -0700

    integrate web server

--- server.js
+++ server.js
@@ -3,8 +3,30 @@ var app = express(); // make express app
 
 const PORT = process.env.PORT || 5000;
 var server = app.listen(PORT, () => console.log('Server is running on PORT: ', PORT));
+var path = require('path');
 
+app.use(express.static(__dirname + '/public'));
 app.use(express.static('public'));
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+app.set('views', path.join(__dirname, 'public/html'));
+//app.set('views', 'public/html');
+
+app.get('/', (req, res) => {
+  res.render('landing.html')
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.html')
+});
+
+app.get('/signup', (req, res) => {
+  res.render('signup.html');
+});
+
+app.get('/canvas', (req, res) => {
+  res.render('canvas.html')
+});
 
 var socket = require('socket.io');
 var io = socket(server);
