f87207e1620313a0c04afb1216c2be945a5a30a4
Ali Zaidi <alizaidi678@gmail.com>
2020-05-18 22:16:41 -0700

    Removed Config

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express'); // import express
 var app = express(); // make express app
 
+require('dotenv').config()
+
 const PORT = process.env.PORT || 5000;
 var server = app.listen(PORT, () => console.log('Server is running on PORT: ', PORT));
 var path = require('path');
