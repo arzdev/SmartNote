848ddbda0f387d4a5c8b4f72d3e6a326b26e8cfb
Ashnfreeman <34043876+Ashnfreeman@users.noreply.github.com>
2020-06-03 22:32:59 -0700

    Add files via upload

--- public/sketch.js
+++ public/sketch.js
@@ -1,198 +1,220 @@
 var socket;
 var user_color;
 var pen_size;
-var database;
-
 var drawing = [];
-var currentPath=[];
-var isDrawing=false;
-// load up drawing function if u can
-var firebase = require('firebase/app');
-require('firebase/auth');
-require('firebase/database');
+var fillingForm = false;
+var placingText = false;
+var input_text = "";
+var font_size = 8;
+var roomId = "";
 
 function setup() {
   // put setup code here
-  console.log('\n\n\ngoing through setup!\n\n\n')
-  canvas=createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
   background(255);
+
   user_color = "#000000";
   pen_size = 20;
+
   socket = io.connect('localhost:5000')
+  socket.on('room', getRoomId);
   socket.on('mouse', newDrawing);
 
-  canvas.mousePressed(startPath);
-  canvas.parent('canvascontainer');
-  canvas.mouseReleased(endPath);
-
-  var saveButton = select('#saveButton');
-  saveButton.mousePressed(saveDrawing);
-
-  var clearButton = select('#clearButton');
-  clearButton.mousePressed(clearDrawing);
-
-
-  var config = {
-      apiKey: "AIzaSyBct07FmogeLJcN0keelvtK9lLrFmSEAb4",
-      authDomain: "smartnote-1590114095005.firebaseapp.com",
-      databaseURL: "https://smartnote-1590114095005.firebaseio.com",
-      projectId: "smartnote-1590114095005",
-      storageBucket: "smartnote-1590114095005.appspot.com",
-      messagingSenderId: "667040488592",
-      appId: "1:667040488592:web:7b70f9deba4fb1f2ba4c15",
-      measurementId: "G-2680TNSY38"
-    };
+  /*
+  var firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  };
   // Initialize Firebase
-  /*firebase.initializeApp(firebaseConfig);
-  database=firebase.database();
+  firebase.initializeApp(firebaseConfig);
   firebase.analytics();
-  console.log(firebase);*/
-  firebase.initializeApp(config);
-  database = firebase.database();
+  console.log(firebase)
+  */
 
-  var params=getURLParams();
-  console.log(params);
-  if(params.id){
-    console.log(params.id);
-    showDrawing(params.id);
-  }
-
-  var ref=database.ref('drawings');
-  ref.on('value', gotData, errData);
 }
 
+function getRoomId(data){
+  roomId = data;
+  console.log('got it!')
+  console.log(roomId);
+}
 
 function newDrawing(data) {
-  stroke(data.color);
-  strokeWeight(data.pensize);
-  line(data.x, data.y, data.xp, data.xy);
+  console.log('new called!')
+  console.log(data)
+  if(data.type === "line"){
+    console.log('no here!')
+    stroke(data.color);
+    strokeWeight(data.pensize);
+    line(data.x, data.y, data.xp, data.xy);
+  }
+  else{
+    console.log('here setting weight!')
+    strokeWeight(1);
+    textSize(data.size)
+    text(data.words, data.x, data.y)
+  }
 }
-
 
 function draw() {
-  // put drawing code here
-
-  if (mouseIsPressed) {
-    stroke(user_color);
-    strokeWeight(pen_size);
-    console.log('Sending: ' + mouseX + ',' + mouseY);
-    line(mouseX, mouseY, pmouseX, pmouseY);
-
-    var data = {
-      x: mouseX,
-      y: mouseY,
-      xp: pmouseX,
-      xy: pmouseY,
-      pensize: pen_size,
-      color: user_color
+  if(mouseIsPressed){
+    if(placingText){
+      console.log(pen_size);
+      textFont('Georgia');
+      textSize(font_size);
+      data = {
+        type: "text",
+        x: mouseX,
+        y: mouseY,
+        words: input_text,
+        size: font_size
+      }
+      console.log('fontsize is: ')
+      console.log(font_size)
+      text(input_text, mouseX, mouseY);
+      socket.emit('mouse', data);
+      drawing.push(data);
+      setTimeout(function(){ placingText = false; }, 500);
     }
-    var point = {
-      x:mouseX,
-      y:mouseY
+    else if(fillingForm == false && placingText == false){
+      makeLine()
     }
-    currentPath.push(point);
-    socket.emit('mouse', data);
-    drawing.push(data);
   }
-  for (var i=0; i<drawing.length;i++){
-    var path=drawing[i];
-    beginShape();
-    for (var j=0; j<path.length;j++){
-      vertex(path[j].x, path[j].y);
+}
+
+function makeLine(){
+
+    if(!placingText){
+      stroke(user_color);
+      strokeWeight(pen_size);
+      line(mouseX, mouseY, pmouseX, pmouseY);
+
+      var data = {
+        type: "line",
+        x: mouseX,
+        y: mouseY,
+        xp: pmouseX,
+        xy: pmouseY,
+        pensize: pen_size,
+        color: user_color
+      }
+      socket.emit('mouse', data);
+      drawing.push(data);
     }
-    endShape();
+}
+
+function textSubmission(e){
+  fillingForm = false;
+  placingText = true;
+  e.preventDefault();
+  input_text = e.target.elements.words.value
+  font_size = parseInt(e.target.elements.font_size.value)
+  document.getElementById("text_form").style.display = "none";
+}
+
+function displayTextSubmission(){
+  if(!fillingForm){
+    fillingForm = true;
+    document.getElementById("text_form").style.display = "block";
+    console.log('before:')
+    console.log(pen_size)
+    strokeWeight(1);
   }
 }
 
-function startPath(){
-  isDrawing=true;
-  mouseIsPressed=true;
-  currentPath=[];
-  drawing.push(currentPath);
+function sliderSubmission(e){
+  e.preventDefault();
+  fillingForm = false;
+  thickness = e.target.elements.penSlider.value
+  strokeWeight(thickness)
+  document.getElementById("slider").style.display = "none";
 }
 
-function endPath(){
-  isDrawing=false;
-  mouseIsPressed=false;
-}
-
-function saveDrawing(){
-  var ref=database.ref('drawings');
-  var data={
-    name: 'placeholder',
-    drawing: drawing //IDK WHAT THIS IS
-  };
-  var result=ref.push(data, dataSent);
-  console.log(result.key);
-
-  function dataSent(err, status){
-    console.log(status);
+function displayThicknessSlider(){
+  if(!fillingForm){
+    slider = document.getElementById("slider")
+    slider.style.display = "block";
+    fillingForm = true;
   }
 }
 
-function gotData(data){
-  var elts=selectAll('.listing');
-  for(var i=0; i<elts.length;i++){
-    elts[i].remove();
-  }
+function changeColor(){
+  var colorButton = document.getElementById("color");
+}
 
-  var drawings=data.val();
-  var keys=Object.keys(drawings);
-  for (var i=0; i<keys.length;i++){
-    var key=keys[i];
-    var li=createElement('li', '');
-    li.class('listing');
-    var ahref=createA('#', key);
-    ahref.mousePressed(showDrawing);
-    ahref.parent(li);
-
-    var perma=createA('?id='+key, 'permalink');
-    perma.parent(li);
-    perma.style('padding', '4px');
-
-    li.parent('drawinglist');
+function displayShareForm(){
+  if(!fillingForm){
+    share_form = document.getElementById("share_form")
+    share_form.style.display = "block";
+    share_form.elements.invite_url.value = "http://localhost:5000/canvas?roomid=" + roomId
+    fillingForm = true;
   }
 }
 
-function errData(err){
-  console.log(err);
+function downloadAsPng(){
+  saveCanvas('myCanvas', 'png');
 }
 
-function showDrawing(key){
-  if(key instanceof MouseEvent){
-    key=this.html();
-  }
-  var ref=database.ref('drawings/'+key);
-  ref.once('value', oneDrawing, errData);
-
-  function oneDrawing(data){
-    var dbdrawing=data.val();
-    drawing=dbdrawing.drawing;
-    console.log(drawing);
-  }
+function downloadAsPdf(){
+  saveCanvas('myCanvas', 'pdf');
 }
 
-function clearDrawing(){
-  drawing=[];
-}
 
-function mousePressed() {
-  let d = 10;
+function submitShareForm(e){
+  e.preventDefault();
+  form = document.getElementById("share_form")
+  url = document.getElementById("invite_url")
+  url.select();
+  document.execCommand("copy");
+  form.style.display = "none";
+  console.log(roomid)
+  fillingForm = false;
 }
-
 
 
 document.addEventListener("DOMContentLoaded", (event) => { // make sure the website is fully downloaded
+
+  var share_icon = document.getElementById("share_icon")
+  var share_form = document.getElementById("share_form")
+  var text_form = document.getElementById("text_form");
+  var text_icon = document.getElementById("text_icon");
+  var pen_icon = document.getElementById("pen_icon");
+  var slider = document.getElementById("slider");
+  var colorButton = document.getElementById("color");
+  var downloadPngButton=document.getElementById("download_png");
+  var downloadPdfButton=document.getElementById("download_pdf");
+
+  colorButton.onChange = changeColor();
+
+  share_icon.onclick = displayShareForm;
+  share_form.style.display = "none";
+  share_form.onsubmit = submitShareForm
+
+  downloadPngButton.onclick=downloadAsPng;
+  downloadPdfButton.onclick=downloadAsPdf;
+
+  slider.addEventListener("submit", sliderSubmission, false);
+  slider.style.display = "none";
+  pen_icon.onclick = displayThicknessSlider;
+  text_form.addEventListener("submit", textSubmission, false);
+  text_form.style.display = "none";
+  text_icon.onclick = displayTextSubmission
 
   // Change color
   var colorHTML = document.getElementById("color");
   colorHTML.oninput = function () {
     user_color = colorHTML.value;
   }
-
   // Slider for Pen Size
   var rangeslider = document.getElementById("penSlider");
   rangeslider.oninput = function () {
     pen_size = rangeslider.value;
   }
-})
+
+});
