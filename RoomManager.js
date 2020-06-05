6a12d3cf90b652b5c03382515affac0ef1b748ef
Ali Zaidi <alizaidi678@gmail.com>
2020-06-04 20:55:58 -0700

    final merged

---
+++ RoomManager.js
@@ -0,0 +1,79 @@
+class RoomManager{
+  constructor(){
+    this.rooms = {}
+    this.socketMap = {}
+  }
+
+  add_room(room_name){
+    if(this.room_exists(room_name)){
+      return this.rooms[room_name]
+    }
+    this.rooms[room_name] = {}//empty json room
+    this.rooms[room_name]["users"] = []
+    this.rooms[room_name]["size"] = 0
+    this.rooms[room_name]["history"] = []
+    return this.rooms[room_name]
+  }
+
+  get_room(room_name){
+    return this.rooms[room_name]
+  }
+
+  get_history(room_name){
+    return this.get_room(room_name)["history"]
+  }
+  
+  room_exists(room_name){
+    return room_name in this.rooms
+  }
+
+  remove_user(user){
+    let room_name = this.socketMap[user]
+    if(typeof room_name !== 'undefined'){
+      let users = this.rooms[room_name]["users"]
+      for(var i = users.length - 1; i >= 0; i--) {
+        if(users[i] === user) {
+          users.splice(i, 1);
+          console.log('disconnect removed!')
+        }
+      }
+    }
+  }
+
+  add_user(room_name, user){
+    let room;
+    this.socketMap[user] = room_name
+    if(!this.room_exists(room_name)){
+      room = this.add_room(room_name)
+    }
+    else{
+      room = this.get_room(room_name);
+    }
+    room["users"].push(user)
+    console.log(room)
+    room["size"] += 1
+  }
+
+  push_stroke(room_name, stroke){
+    this.get_history(room_name).push({"stroke": stroke})
+  }
+
+  get_host_socket(room_name){
+    console.log('sockets in room: ' + this.rooms[room_name]["users"])
+    return this.rooms[room_name]["users"][0]
+  }
+}
+
+class Singleton{
+  constructor(){
+    if (!Singleton.instance) {
+        Singleton.instance = new RoomManager();
+    }    
+  }
+
+  getInstance(){
+    return Singleton.instance
+  }
+}
+
+module.exports = Singleton 
---
+++ public/sketch.js
@@ -0,0 +1,211 @@
+var socket;
+var user_color;
+var pen_size;
+var drawing = [];
+var fillingForm = false;
+var placingText = false;
+var input_text = "";
+var font_size = 8;
+var roomId = "";
+
+function setup() {
+	// put setup code here
+	createCanvas(windowWidth, windowHeight);
+	background(255);
+
+	user_color = "#000000";
+	pen_size = 20;
+
+	socket = io.connect("localhost:5000");
+	socket.on("room", getRoomId);
+	socket.on("mouse", newDrawing);
+}
+
+function getRoomId(data) {
+	roomId = data;
+	console.log("got it!");
+	console.log(roomId);
+}
+
+function newDrawing(data) {
+	console.log("new called!");
+	console.log(data);
+	if (data.type === "line") {
+		console.log("no here!");
+		stroke(data.color);
+		strokeWeight(data.pensize);
+		line(data.x, data.y, data.xp, data.xy);
+	} else {
+		console.log("here setting weight!");
+		strokeWeight(1);
+		textSize(data.size);
+		text(data.words, data.x, data.y);
+	}
+}
+
+function draw() {
+	if (mouseIsPressed) {
+		if (placingText) {
+			console.log(pen_size);
+			textFont("Georgia");
+			textSize(font_size);
+			data = {
+				type: "text",
+				x: mouseX,
+				y: mouseY,
+				words: input_text,
+				size: font_size,
+			};
+			console.log("fontsize is: ");
+			console.log(font_size);
+			text(input_text, mouseX, mouseY);
+			socket.emit("mouse", data);
+			drawing.push(data);
+			setTimeout(function () {
+				placingText = false;
+			}, 500);
+		} else if (fillingForm == false && placingText == false) {
+			makeLine();
+		}
+	}
+}
+
+function makeLine() {
+	if (!placingText) {
+		stroke(user_color);
+		strokeWeight(pen_size);
+		line(mouseX, mouseY, pmouseX, pmouseY);
+
+		var data = {
+			type: "line",
+			x: mouseX,
+			y: mouseY,
+			xp: pmouseX,
+			xy: pmouseY,
+			pensize: pen_size,
+			color: user_color,
+		};
+		socket.emit("mouse", data);
+		drawing.push(data);
+	}
+}
+
+function sliderSubmission(e) {
+	e.preventDefault();
+	fillingForm = false;
+	thickness = e.target.elements.penSlider.value;
+	strokeWeight(thickness);
+	document.getElementById("slider").style.display = "none";
+}
+
+function textSubmission(e) {
+	fillingForm = false;
+	placingText = true;
+	e.preventDefault();
+	input_text = e.target.elements.words.value;
+	font_size = parseInt(e.target.elements.font_size.value);
+	document.getElementById("text_form").style.display = "none";
+}
+
+function displayTextSubmission() {
+	if (!fillingForm) {
+		fillingForm = true;
+		document.getElementById("text_form").style.display = "block";
+		console.log("before:");
+		console.log(pen_size);
+		strokeWeight(1);
+	}
+}
+
+function displayThicknessSlider() {
+	if (!fillingForm) {
+		slider = document.getElementById("slider");
+		slider.style.display = "block";
+		fillingForm = true;
+	}
+	curr_color = document.getElementById("color");
+	user_color = curr_color.value;
+}
+
+function changeColor() {
+	var colorButton = document.getElementById("color");
+}
+
+function displayShareForm() {
+	if (!fillingForm) {
+		share_form = document.getElementById("share_form");
+		share_form.style.display = "block";
+		share_form.elements.invite_url.value =
+			"http://localhost:5000/canvas?roomid=" + roomId;
+		fillingForm = true;
+	}
+}
+
+function downloadAsPng() {
+	saveCanvas("myCanvas", "png");
+}
+
+function downloadAsJpeg() {
+	saveCanvas("myCanvas", "jpeg");
+}
+
+function submitShareForm(e) {
+	e.preventDefault();
+	form = document.getElementById("share_form");
+	url = document.getElementById("invite_url");
+	url.select();
+	document.execCommand("copy");
+	form.style.display = "none";
+	console.log(roomid);
+	fillingForm = false;
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+	// make sure the website is fully downloaded
+
+	var share_icon = document.getElementById("share_icon");
+	var share_form = document.getElementById("share_form");
+	var text_form = document.getElementById("text_form");
+	var text_icon = document.getElementById("text_icon");
+	var pen_icon = document.getElementById("pen_icon");
+	var slider = document.getElementById("slider");
+	var colorButton = document.getElementById("color");
+	var downloadPngButton = document.getElementById("download_png");
+	var downloadJpegButton = document.getElementById("download_Jpeg");
+	var eraser = document.getElementById("eraser");
+
+	colorButton.onChange = changeColor();
+
+	share_icon.onclick = displayShareForm;
+	share_form.style.display = "none";
+	share_form.onsubmit = submitShareForm;
+
+	downloadPngButton.onclick = downloadAsPng;
+	downloadJpegButton.onclick = downloadAsJpeg;
+
+	slider.addEventListener("submit", sliderSubmission, false);
+	slider.style.display = "none";
+
+	text_form.addEventListener("submit", textSubmission, false);
+	text_form.style.display = "none";
+	text_icon.onclick = displayTextSubmission;
+
+	pen_icon.onclick = displayThicknessSlider;
+
+	// eraser
+	eraser.onclick = function () {
+		user_color = 0xffffff;
+	};
+
+	// Change color
+	var colorHTML = document.getElementById("color");
+	colorHTML.oninput = function () {
+		user_color = colorHTML.value;
+	};
+
+	// Slider for Pen Size
+	var rangeslider = document.getElementById("penSlider");
+	rangeslider.oninput = function () {
+		pen_size = rangeslider.value;
+	};
+});
